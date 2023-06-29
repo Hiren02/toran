7bcdc00cb84db74d67fbe7a9800fa0d00bfd8cff
Hiren@0228 <hirenchavda@gmail.com>
2023-06-30 02:16:53 +0530

    Modify:booking page and added rules modal

--- app/booking/page.jsx
+++ app/booking/page.jsx
@@ -1,32 +1,38 @@
 "use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import CartImage from '../../public/Images/cart.png'
 import { Controller, useForm } from 'react-hook-form';
 import DatePicker from 'react-date-picker'
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
-import TimeSlotSlider from '@/components/TimeSlotSlider/page';
+import RulesModal from '@/components/FeedBackModal/page';
+
 export default function Booking() {
   const { control } = useForm()
-
+  const [show, setShow] = useState(false)
+  const onShowModal = () => {
+    setShow(true)
+  }
+  const onHideModal = () => {
+    setShow(false)
+  }
   return (
     <div className='pt-36 pb-36'>
       <div className="flex justify-center h-100">
         <div className="w-4/5 my-5 mx-auto flex bg-gray-100 flex-col items-center md:flex-row ">
           <div className="border w-[70%] h-full  text-orange-500 p-10"> <span>
-            <h1 className="text-3xl font-semibold text-center mb-4 text-orange-400">Confirm Your Booking</h1>
+            <h1 className="text-3xl font-semibold text-center mb-4 text-orange-400 pt-3">Confirm Your Booking</h1>
             {/* <p className="text-xs font-normal mt-2 text-center">Contact us by filling this contact form. </p> */}
           </span>
             <form>
               <div className='sm:flex'>
-
                 <div className='w-full sm:w-[calc(100%_-_130px)]'>
                   <div className='mt-5'>
                     <div className='grid-cols-1 gap-x-4 md:grid-cols-2'>
                       <div className='col-span-6'>
                         <div className='mb-4'>
-                          <label className='pl-4 text-lg'>Select Sports</label>
+                          <label className='pl-4 text-md'>Select Sports</label>
                           {/* <div className='rounded-xl pt-8 border-spacing-3 px-2 py-2 w-full border-2 border-midextralightgray'> */}
                           <select id="countries" className="text-black mt-1 rounded-xl border-spacing-3 px-2 py-2 w-full border-2 border-midextralightgray">
                             <option selected className='text-black' >Cricket</option>
@@ -37,7 +43,7 @@ export default function Booking() {
                       </div>
                       <div className='col-span-6 rounded-2xl'>
                         <div className='mb-4'>
-                          <label className='pl-4 text-lg'>Select Date</label>
+                          <label className='pl-4 text-md'>Select Date</label>
                           <Controller
                             control={control}
                             name='DOB'
@@ -61,11 +67,11 @@ export default function Booking() {
                       </div>
                       <div className='col-span-6'>
                         <div className='mb-4'>
-                          <label className='pl-4 pb-3'>Select Time slot</label>
-                          <div className='rounded-xl pt-2 border-spacing-3 px-2 py-2 w-full border-2 border-midextralightgray'>
+                          <label className='pl-4 pb-3 text-md'>Time slot</label>
+                          <div className='rounded-xl p-3 border-spacing-3 w-full border-2 border-midextralightgray'>
                             {/* <select multiple> */}
                             {/* {/* <option className='bg-white rounded-xl p-2 m-1'>6-7AM</option> */}
-                            <span className='bg-white rounded-xl p-2 m-1'>6-7AM</span>
+                            <span className='bg-white text-black border-gray-400 border-2   rounded-xl p-2 m-1'>10-11 PM</span>
                             {/* </select> */}
                           </div>
                         </div>
@@ -75,14 +81,16 @@ export default function Booking() {
                 </div>
               </div>
               <div className="mt-6 flex items-center justify-center">
-                <button type='submit' className={`inline-block primary-buttons text-black bg-orange-400 font-bold text-sm bg-primary border border-primary uppercase rounded-2xl px-5 py-3 mr-4 hover:transition-all hover:border-spacing-5 hover:bg-orange-500 lg:text-base `}><span className='button-label'>SAVE & SUBMIT</span></button>
+                <button type='submit' className={`inline-block primary-buttons text-black bg-orange-400 font-bold text-sm bg-primary border border-primary uppercase rounded-2xl px-5 py-3 mr-4 hover:transition-all hover:border-spacing-5 hover:bg-orange-500 lg:text-base `}><span className='button-label'>Add to cart</span></button>
               </div>
             </form>
           </div>
-          <div className="border w-[70%] md:w-[40%] bg-gray-100 items-center flex flex-col justify-center leading-7 space-y-8 h-full">
-            {/* <Image src={CartImage} alt="Cart" className="" /> */}
+          <div className="border lg:pt-4 w-[70%] md:w-[40%] bg-gray-100 items-center flex flex-col justify-center leading-7 space-y-8 h-full">
             {/* <h3 className="text-black">Your Cart is empty</h3> */}
-            <h2 className='text-orange-400 font-extrabold text-2xl'> Your cart</h2>
+            <div>
+              <Image src={CartImage} height={70} width={70} alt="Cart" className="rounded-full justify-center" />
+              <h2 className='text-orange-400 font-extrabold text-3xl pr-5'> Your cart</h2>
+            </div>
             <div>
               <label className='font-bold pr-6 text-xl'>Sports: </label>
               <span className='text-xl p-1'>Cricket</span>
@@ -100,11 +108,12 @@ export default function Booking() {
               <span className='text-xl  p-1'>₹ 500</span>
             </div>
             <div>
-              <button className='text-2xl font-bold text-black rounded-xl m-1 p-2 border-orange-400 bg-orange-400'>Procced to pay</button>
+              <button className='inline-block primary-buttons text-black bg-orange-400 font-bold text-sm bg-primary border border-primary uppercase rounded-2xl px-5 py-3 mr-4 hover:transition-all hover:border-spacing-5 hover:bg-orange-500 lg:text-base' onClick={() => onShowModal()}>Procced to pay</button>
             </div>
           </div>
         </div>
       </div>
+      {show ? <RulesModal onHideModal={onHideModal} /> : <></>}
     </div>
   )
 }
---
+++ components/FeedBackModal/page.jsx
@@ -0,0 +1,84 @@
+'use client'
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XCloseIcn } from './Icon/page'
+
+const RulesModal = ({ onHideModal }) => {
+  const [isOpenFeedBackModal, setIsOpenFeedBackModal] = useState(true)
+  const onClose = () => {
+    setIsOpenFeedBackModal(false)
+    onHideModal()
+  }
+  const onContinue = () => {
+
+  }
+  return (
+    <>
+      <Transition appear show={isOpenFeedBackModal} as={Fragment}>
+        <Dialog as="div" className="relative z-20" onClose={() => null}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="relative w-full max-w-2xl transform rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <button
+                    type="button"
+                    className="absolute right-4 top-4 focus:outline-none"
+                    onClick={onClose}
+                  >
+                    <XCloseIcn className='w-6 h-6' />
+                  </button>
+                  <Dialog.Title
+                    as="h3"
+                    className="text-2xl font-bold text-center"
+                  >
+                    Rules
+                  </Dialog.Title>
+                  <div className="mt-5">
+                    <div className='py-5 sm:flex'>
+                      <div className='mr-8 mb-4'>
+                      </div>
+                      <div className='patient-details sm:w-[calc(100%_-_104px)] border-b-2 border-midextralightgray'>
+                        <div className='flex mb-3 w-full'>
+                          <div className='text-semilightgray pr-3 w-1/3 text-base md:text-lg'>Rules 1</div>
+                          <div className='font-bold w-2/3 text-base md:text-lg'></div>
+                        </div>
+                        <div className='flex mb-3 w-full'>
+                          <div className='text-semilightgray pr-3 w-1/3 text-base md:text-lg'>Rules 2</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='pl-64'>
+                    <button className='text-center bg-orange-400 border-2 text-xl rounded-xl p-2 m-1' onClick={onContinue}>Continue</button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
+
+export default RulesModal
