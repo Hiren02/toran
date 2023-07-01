a88d42fd4c8cc3fcd683b939441c75aa11060d2b
Hiren@0228 <hirenchavda@gmail.com>
2023-07-01 12:09:52 +0530

    Added: metadata

--- app/layout.js
+++ app/layout.js
@@ -2,23 +2,15 @@ import Header from '@/components/Header/page'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer/page'
-import Script from 'next/script'
 import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Toran club',
-  description: 'Toran club',
-}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className='h-full '>
       <body className={`${inter.className} h-full`} >
         <NextTopLoader height={5} color="#000000" showSpinner={false} />
-        <Script src="../path/to/flatpickr.min.js"></Script>
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.6/datepicker.min.js"></Script>
         <Header/>
         {children}
         <Footer/>
